fa7f3f3ec0a03c33626f91a1bd56a83e292f7b92
Michael Meschenmoser <michael@mimeit.de>
2019-09-08 11:20:33 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # fritz_temp
+
+A munin plugin for monitoring the temperatures of Smart Home devices
+
+## Passwords
+
+Create a file called `passwords.inc.php` with the following content:
+
+```php
+<?php
+
+$fritz_host = 'FRITZ_BOX_IP_OR_fritz.box';
+$fritz_url = 'https://' . $fritz_host . '/login_sid.lua';
+$auto_url = 'https://' . $fritz_host . '/webservices/homeautoswitch.lua';
+$fritz_user = 'USERNAME';
+$fritz_pwd = 'PASSWORD';
+
+?>
+```
+
+The user must have Smart Home privileges. Other user rights are not needed.
---
+++ fritz_temp.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php
+<?php
+
+include 'passwords.inc.php';
+
+$config = (isset($argv[1]) && $argv[1] == 'config') ? true : false;
+
+$tmp = simplexml_load_string(file_get_contents($fritz_url));
+
+if ($tmp->BlockTime > 0) {
+	trigger_error('BlockTime was set', E_USER_ERROR);
+}
+
+$challenge = $tmp->Challenge;
+
+$challenge_str = $challenge . '-' . $fritz_pwd;
+$md_str = md5(iconv("UTF-8", "UTF-16LE", $challenge_str));
+$response = $challenge . '-' . $md_str;
+$tmp = simplexml_load_string(file_get_contents($fritz_url . '?user=' . $fritz_user . '&response=' . $response));
+
+$sid = $tmp->SID;
+
+$tmp = simplexml_load_string(file_get_contents($auto_url . '?switchcmd=getdevicelistinfos&sid=' . $sid));
+
+if ($config) {
+	echo 'graph_title Temperatures' . PHP_EOL;
+	echo 'graph_vlabel degrees Celsius' . PHP_EOL;
+}
+
+foreach ($tmp->device as $device) {
+	if (!isset($device->temperature)) continue;
+	if ($config) {
+		echo preg_replace('/[^a-zA-Z]/', '', $device->name);
+		echo '.label ';
+		echo preg_replace(array('/ß/', '/ä/', '/ö/', '/ü/'), array('ss', 'ae', 'oe', 'ue'), $device->name);
+	} else {
+		echo preg_replace('/[^a-zA-Z]/', '', $device->name);
+		echo '.value ';
+		echo ( $device->temperature->celsius / 10);
+	}
+	echo PHP_EOL;
+}
+
+$tmp = simplexml_load_string(file_get_contents($fritz_url . '?logout=1&sid=' . $sid));
+
+?>
